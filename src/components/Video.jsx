b384c0f7e9555c4e87b8617e25c1e10a456ee000
Sanysh <galanin09234@mail.ru>
2024-01-25 15:44:18 +0300

    add id

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import {DateTimePretty} from './DateTime';
 
-const Video = ({url, date}) => {
+const Video = ({url, date, id}) => {
   return (
     <div className="video">
-      <iframe src={url} style={{border: 0, }} allow="autoplay; encrypted-media" allowFullScreen></iframe>
+      <iframe src={url} style={{border: 0, }} allow="autoplay; encrypted-media" allowFullScreen title={id}></iframe>
       <DateTimePretty date={date} />
     </div>
   );
